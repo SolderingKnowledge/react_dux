1a66ddd1081a6f78165cded1097e5ef80e51d330
Aikol Tashbolot uulu <imaikol@yahoo.com>
2019-11-21 22:01:43 -0800

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    const {posts}= this.props;
+    console.log("aikol", posts);
+    const theme = posts.map(post=>(
+      <div key = {post.id}>{post.title}</div>
+    ))
+    return (
+      <div className="App">
+        {theme}
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state)=>{//state is the reducers
+  console.log("ok", state)
+  return {
+    posts: state
+  }
+}
+
+export default connect(mapStateToProps)(App);
